7e4912c79a0b97da02ad996ceb0751307406b373
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-06-15 21:57:56 +0300

    feat: custom error
    add custom error and update error handling

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,9 @@
+const { CustomAPIError } = require('../errors/custom-error');
 const errorHandlerMiddleware = (err, req, res, next) => {
-  return res.status(500).json({ status: 'failed', message: err.message });
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+  return res.status(500).json({ message: 'Unexpected error' });
 };
 
 module.exports = errorHandlerMiddleware;
